eccaca27c12bae8a4d11624911b29abaf7e28f5b
corner <corner3499@gmail.com>
2023-09-06 16:27:36 +0900

    post page 스타일링

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,18 +1,32 @@
 import { getPostData } from '@/service/posts';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import MarkdownViewer from '@/components/MarkdownViewer';
+import Image from 'next/image';
+import { AiTwotoneCalendar } from 'react-icons/ai';
 
 type Props = {
     params: {
         slug: string;
     };
 };
+const markdown = `Just a link: https://reactjs.com.`;
 export default async function PostPage({ params: { slug } }: Props) {
-    // 전달된 slug에 해당하는 포스트 데이터 읽기
-    // 데이터를 마크다운뷰어에 표기
-    const post = await getPostData(slug);
+    const { path, content, title, description, date, featured, category } = await getPostData(slug);
+
     return (
-        <>
-            <h1>{post.title}</h1>
-            <pre>{post.content}</pre>
-        </>
+        <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+            <Image className="w-full h-1/5 max-h-[500px]" src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
+            <section className="flex flex-col p-4">
+                <div className="flex items-center self-end text-sky-600">
+                    <AiTwotoneCalendar />
+                    <p className="font-semibold ml-2">{date.toString()}</p>
+                </div>
+                <h1 className="text-4xl font-bold">{title}</h1>
+                <p className="text-xl font-bold">{description}</p>
+                <div className="w-44 border-2 border-sky-600 mt-4 mb-8"></div>
+                <MarkdownViewer content={content} />
+            </section>
+        </article>
     );
 }
