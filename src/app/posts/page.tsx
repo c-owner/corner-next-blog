ed7e012510bc40464728b653765a774ca0bdf53d
corner <corner3499@gmail.com>
2023-09-06 14:21:59 +0900

    filter post page & category ✅

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,8 @@
-export default function PostsPage() {
-    return <section>PostsPage</section>;
+import { getAllPosts } from '@/service/posts';
+import FilterablePosts from '@/components/FilterablePosts';
+
+export default async function PostsPage() {
+    const posts = await getAllPosts();
+    const categories = [...new Set(posts.map((post) => post.category))];
+    return <FilterablePosts posts={posts} categories={categories} />;
 }
---
+++ src/components/FilterablePosts.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Post } from '@/service/posts';
+import { useState } from 'react';
+import PostsGrid from '@/components/PostsGrid';
+import Categories from '@/components/Categories';
+
+type Props = {
+    posts: Post[];
+    categories: string[];
+};
+
+const ALL_POSTS = 'All Posts';
+export default function FilterablePosts({ posts, categories }: Props) {
+    const [selected, setSelected] = useState(ALL_POSTS);
+    const filtered = selected === ALL_POSTS ? posts : posts.filter((post) => post.category === selected);
+
+    return (
+        <section>
+            <PostsGrid posts={filtered} />
+            <Categories categories={[ALL_POSTS, ...categories]} selected={selected} onClick={setSelected} />
+        </section>
+    );
+}
