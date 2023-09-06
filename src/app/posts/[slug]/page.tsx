10eb52a627cc06cd570cd9ac977ac4fa8f220a3b
corner <corner3499@gmail.com>
2023-09-06 16:41:17 +0900

    markdown viewer custom ⭐️

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,4 @@
 import { getPostData } from '@/service/posts';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
 import MarkdownViewer from '@/components/MarkdownViewer';
 import Image from 'next/image';
 import { AiTwotoneCalendar } from 'react-icons/ai';
@@ -16,7 +14,13 @@ export default async function PostPage({ params: { slug } }: Props) {
 
     return (
         <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
-            <Image className="w-full h-1/5 max-h-[500px]" src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
+            <Image
+                className="w-full h-1/5 max-h-[500px]"
+                src={`/images/posts/${path}.png`}
+                alt={title}
+                width={760}
+                height={420}
+            />
             <section className="flex flex-col p-4">
                 <div className="flex items-center self-end text-sky-600">
                     <AiTwotoneCalendar />
