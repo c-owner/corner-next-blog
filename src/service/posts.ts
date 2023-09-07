d5b3ab780d636931d38f43ae7f49eb51b7f91f0f
corner <corner3499@gmail.com>
2023-09-07 18:03:51 +0900

    cache 성능 개선

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,5 +1,6 @@
 import { readFile } from 'fs/promises';
 import path from 'path';
+import { cache } from 'react';
 
 export type Post = {
     title: string;
@@ -27,12 +28,16 @@ export async function getNonFeaturedPosts(): Promise<Post[]> {
         .then((posts) => posts.filter((post) => !post.featured));
 }
 
-export async function getAllPosts(): Promise<Post[]> {
+/*
+ * cache 사용으로 한 페이지에서의 중복 호출을 방지하며 성능 개선.
+ * cache 함수는 한 페이지 한해서만 캐싱이 되므로, 다른 페이지에서는 다시 호출이 됨
+ */
+export const getAllPosts = cache(async () => {
     const filePath = path.join(process.cwd(), 'data', 'posts.json');
     return readFile(filePath, 'utf-8')
         .then<Post[]>(JSON.parse)
         .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
-}
+});
 
 export async function getPostData(fileName: string): Promise<PostData> {
     const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
